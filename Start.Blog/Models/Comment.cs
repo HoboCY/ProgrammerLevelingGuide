9a3578bd6ea6f4af4a95701ec6bec756fd5272ef
hobocy <1830231903@qq.com>
2021-06-01 21:37:05 +0800

    add models

---
+++ Start.Blog/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Start.Blog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+
+        public string Content { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public int UserId { get; set; }
+
+        public int PostId { get; set; }
+    }
+}
