9e05e4c294d54485b723558b6f06aff88fc7a85f
HoboCY <1830231903@qq.com>
2021-06-02 12:34:33 +0800

    add user controller

---
+++ Start.Blog/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Start.Blog.Helpers;
+using Start.Blog.Models;
+
+namespace Start.Blog.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ISqlHelper<User> _sqlHelper;
+
+        public UserController(ISqlHelper<User> sqlHelper)
+        {
+            _sqlHelper = sqlHelper;
+        }
+    }
+}
