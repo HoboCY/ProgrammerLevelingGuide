484d64bc45584919deb74a87df2e69afecc75682
HoboCY <1830231903@qq.com>
2021-06-02 12:33:53 +0800

    add managers

---
+++ Start.Blog/Managers/IUserManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Start.Blog.Models;
+
+namespace Start.Blog.Managers
+{
+    public interface IUserManager<T>
+    {
+        Task<T> FindByIdAsync(int id);
+
+        Task<T> FindByNameAsync(string name);
+    }
+}
---
+++ Start.Blog/Managers/UserManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Start.Blog.Extensions;
+using Start.Blog.Helpers;
+
+namespace Start.Blog.Managers
+{
+    public class UserManager<T> : IUserManager<T>
+    {
+        private readonly ISqlHelper<T> _sqlHelper;
+
+        public UserManager(ISqlHelper<T> sqlHelper)
+        {
+            _sqlHelper = sqlHelper;
+        }
+
+        public async Task<T> FindByIdAsync(int id)
+        {
+            var user = await _sqlHelper.GetAsync(id);
+            return user;
+        }
+
+        public async Task<T> FindByNameAsync(string name)
+        {
+            if (name.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(name), "Invalid parameters");
+            var parameters = new DynamicParameters();
+            parameters.Add("Name", name);
+            var user = await _sqlHelper.GetAsync(parameters);
+            return user;
+        }
+    }
+}
