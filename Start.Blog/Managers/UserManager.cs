b8e781a157d79157706d2f58eabb73f3372d169f
HoboCY <1830231903@qq.com>
2021-06-02 14:24:16 +0800

    add user manager method

--- Start.Blog/Managers/UserManager.cs
+++ Start.Blog/Managers/UserManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.AspNetCore.Components.Forms;
 using Start.Blog.Extensions;
 using Start.Blog.Helpers;
+using Start.Blog.Models;
 
 namespace Start.Blog.Managers
 {
-    public class UserManager<T> : IUserManager<T>
+    public class UserManager<T> : IUserManager<T> where T : IUser, new()
     {
         private readonly ISqlHelper<T> _sqlHelper;
 
@@ -25,11 +27,24 @@ namespace Start.Blog.Managers
 
         public async Task<T> FindByNameAsync(string name)
         {
-            if (name.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(name), "Invalid parameters");
             var parameters = new DynamicParameters();
             parameters.Add("Name", name);
             var user = await _sqlHelper.GetAsync(parameters);
             return user;
+        }
+
+        public Task<bool> CheckPasswordAsync(T t, string password, string salt = null)
+        {
+            var input = salt.IsNullOrWhiteSpace() ? password : password + salt;
+            var result = t.Password == input.ToMd5();
+            return Task.FromResult(result);
+        }
+
+        public async Task RegisterAsync(string name, string password, string salt = null)
+        {
+            var inputPassword = salt.IsNullOrWhiteSpace() ? password : password + salt;
+            var user = new T {Name = name, Password = inputPassword.ToMd5()};
+            await _sqlHelper.AddAsync(user);
         }
     }
 }
