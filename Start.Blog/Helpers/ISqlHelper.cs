991569d76a11639850ec45e2b660c61d97cdb6e9
HoboCY <1830231903@qq.com>
2021-06-02 12:33:35 +0800

    add a GetAsync Method

--- Start.Blog/Helpers/ISqlHelper.cs
+++ Start.Blog/Helpers/ISqlHelper.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using MySqlConnector;
 
 namespace Start.Blog.Helpers
 {
     public interface ISqlHelper<T>
     {
         Task<T> GetAsync(int id);
+
+        Task<T> GetAsync(DynamicParameters parameters);
 
         Task<IEnumerable<T>> GetAsync(Func<T, bool> expression);
 
--- Start.Blog/Helpers/MysqlHelper.cs
+++ Start.Blog/Helpers/MysqlHelper.cs
@@ -13,26 +13,45 @@ namespace Start.Blog.Helpers
     public class MysqlHelper<T> : ISqlHelper<T>
     {
         private readonly string _connectionString;
-        private readonly string typeName;
+        private readonly string _typeName;
 
         public MysqlHelper(IConfiguration configuration)
         {
             _connectionString = configuration.GetConnectionString("Default");
-            typeName = typeof(T).Name;
+            _typeName = typeof(T).Name;
         }
 
         public async Task<T> GetAsync(int id)
         {
             await using var conn = new MySqlConnection(_connectionString);
-            var sql = $"SELECT * FROM {typeName} WHERE Id = @Id";
-            var entity = await conn.QueryFirstOrDefaultAsync<T>(sql, new { id });
+            var sql = $"SELECT * FROM {_typeName} WHERE Id = @Id";
+            var entity = await conn.QuerySingleOrDefaultAsync<T>(sql, new { id });
             return entity == null ? throw new SqlNullValueException($"Not found {typeof(T).Name} entity with id:{id}") : entity;
+        }
+
+        public async Task<T> GetAsync(DynamicParameters parameters)
+        {
+            if (parameters == null || !parameters.ParameterNames.Any()) throw new NullReferenceException("Invalid parameters");
+            await using var conn = new MySqlConnection(_connectionString);
+            var sql = new StringBuilder($"SELECT * FROM {_typeName} WHERE ");
+
+            var parameterNames = parameters.ParameterNames.ToList();
+
+            foreach (var name in parameterNames)
+            {
+                sql.Append($"{name}=@{name}");
+                if (parameterNames.Last() != name)
+                    sql.Append(" AND ");
+            }
+
+            var entity = await conn.QuerySingleOrDefaultAsync<T>(sql.ToString(), parameters);
+            return entity == null ? throw new SqlNullValueException($"Not found {typeof(T).Name} entity") : entity;
         }
 
         public async Task<IEnumerable<T>> GetAsync(Func<T, bool> expression)
         {
             await using var conn = new MySqlConnection(_connectionString);
-            var sql = $"SELECT * FROM {typeName}";
+            var sql = $"SELECT * FROM {_typeName}";
             var entities = (await conn.QueryAsync<T>(sql)).Where(expression).ToList();
             return !entities.Any() ? new List<T>() : entities;
         }
@@ -41,7 +60,7 @@ namespace Start.Blog.Helpers
         {
             await using var conn = new MySqlConnection(_connectionString);
             var properties = typeof(T).GetProperties().Where(p => p.Name != "Id").Select(p => p.Name).ToList();
-            var sql = $"INSERT INTO {typeName} ({string.Join(",", properties)}) VALUES ({string.Join(",", properties.Select(p => $"@{p}"))})";
+            var sql = $"INSERT INTO {_typeName} ({string.Join(",", properties)}) VALUES ({string.Join(",", properties.Select(p => $"@{p}"))})";
             var count = await conn.ExecuteAsync(sql, entity);
             if (count <= 0) throw new InvalidOperationException("INSERT FAILED");
         }
@@ -50,7 +69,7 @@ namespace Start.Blog.Helpers
         {
             await using var conn = new MySqlConnection(_connectionString);
             var properties = typeof(T).GetProperties().Where(p => p.Name != "Id").Select(p => $"{p.Name}=@{p.Name}").ToList();
-            var sql = $"UPDATE {typeName} SET {string.Join(",", properties)} WHERE Id = @Id";
+            var sql = $"UPDATE {_typeName} SET {string.Join(",", properties)} WHERE Id = @Id";
             var count = await conn.ExecuteAsync(sql, entity);
             if (count <= 0) throw new InvalidOperationException("UPDATE FAILED");
         }
@@ -58,7 +77,7 @@ namespace Start.Blog.Helpers
         public async Task DeleteAsync(int id)
         {
             await using var conn = new MySqlConnection(_connectionString);
-            var sql = $"DELETE FROM {typeName} WHERE Id = @Id";
+            var sql = $"DELETE FROM {_typeName} WHERE Id = @Id";
             var count = await conn.ExecuteAsync(sql, new { id });
             if (count <= 0) throw new InvalidOperationException("DELETE FAILED");
         }
