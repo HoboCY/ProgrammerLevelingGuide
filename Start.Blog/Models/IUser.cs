b12f24a540a14674abe03cdb19712765641e22a4
HoboCY <1830231903@qq.com>
2021-06-02 14:23:43 +0800

    add user interface

---
+++ Start.Blog/Models/IUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Start.Blog.Models
+{
+    public interface IUser
+    {
+        string Name { get; set; }
+
+        string Password { get; set; }
+    }
+}
