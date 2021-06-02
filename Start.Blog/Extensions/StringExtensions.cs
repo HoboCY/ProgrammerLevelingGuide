ec7dec357da6e69e8a843102d71bc985014ac1ab
HoboCY <1830231903@qq.com>
2021-06-02 12:34:09 +0800

    add a string extension method

---
+++ Start.Blog/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Start.Blog.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string text)
+        {
+            return string.IsNullOrWhiteSpace(text);
+        }
+    }
+}
