d01931af930af61511fabe87393fbcb21c01aae9
HoboCY <1830231903@qq.com>
2021-06-16 16:33:32 +0800

    complete register function and view

---
+++ Start.Blog/wwwroot/js/register.js
@@ -0,0 +1,18 @@
+﻿$("#BtnRegister").click(function () {
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var confirmPassword = $("#confirmPassword").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/api/user/register",
+        data: JSON.stringify({ username, password, confirmPassword }),
+        contentType: "application/json",
+        success: function (data) {
+            alert(data);
+        },
+        error: function (error) {
+            alert(error.responseText);
+        }
+    });
+});
