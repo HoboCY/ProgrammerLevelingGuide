1afc07e2723a0add67d84f679e424f9c5d5d1945
HoboCY <1830231903@qq.com>
2021-06-21 17:53:20 +0800

    add post controller and complete create function

---
+++ Start.Blog/Controllers/PostController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Start.Blog.Helpers;
+using Start.Blog.Models;
+using Start.Blog.Services;
+using Start.Blog.ViewModels;
+
+namespace Start.Blog.Controllers
+{
+    [ApiController]
+    public class PostController : Controller
+    {
+        private readonly ISqlHelper<Post> _sqlHelper;
+        private readonly IUserService _userService;
+
+        public PostController(ISqlHelper<Post> sqlHelper,IUserService userService)
+        {
+            _sqlHelper = sqlHelper;
+            _userService = userService;
+        }
+
+        [HttpGet("[controller]/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("api/[controller]")]
+        public async Task<IActionResult> CreateAsync(CreateBlogInput input)
+        {
+            await _sqlHelper.AddAsync(new Post
+            {
+                Title = input.Title,
+                Content = input.Content,
+                CreationTime = DateTime.Now,
+                UserId = _userService.GetUserId()
+            });
+            return NoContent();
+        }
+    }
+}
---
+++ Start.Blog/ViewModels/CreateBlogInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Start.Blog.ViewModels
+{
+    public class CreateBlogInput
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
